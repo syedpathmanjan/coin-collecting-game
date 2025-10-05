7eaa8b820285c8ea23c3fee896ec04a830fd6940
syedpathmanjan <syedpathmanjan@protonmail.com>
2025-10-05 16:58:57 +0000

    Add world class and draw grid

--- main.js
+++ main.js
@@ -6,5 +6,29 @@ const GAME_HEIGHT = TILE_SIZE * ROWS;
 
 window.addEventListener('load', () => {
     const canvas = document.getElementById('canvas1')
-    const ctx = canvas.getContext()
+    const ctx = canvas.getContext('2d');
+
+    canvas.width = GAME_WIDTH;
+    canvas.height = GAME_HEIGHT;
+    const world = new World();
+    world.drawGrid(ctx);
 })
+
+class World {
+    constructor() {
+        this.level1 = {}
+    }
+
+    drawGrid(ctx){
+        for (let row = 0; row < ROWS; row++) {
+            for (let col = 0; col < COLS; col++) {
+                ctx.strokeRect(
+                    col * TILE_SIZE,
+                    row * TILE_SIZE,
+                    TILE_SIZE,
+                    TILE_SIZE,
+                )
+            }
+        }
+    }
+}
