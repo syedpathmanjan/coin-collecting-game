083c803590e36d0c2183e1ccc57b780f5fe7b571
syedpathmanjan <syedpathmanjan@protonmail.com>
2025-10-05 15:24:05 +0000

    Basic Setup

---
+++ main.js
@@ -0,0 +1,10 @@
+const TILE_SIZE = 32;
+const COLS = 15;
+const ROWS = 20;
+const GAME_WIDTH = TILE_SIZE * COLS;
+const GAME_HEIGHT = TILE_SIZE * ROWS;
+
+window.addEventListener('load', () => {
+    const canvas = document.getElementById('canvas1')
+    const ctx = canvas.getContext()
+})
